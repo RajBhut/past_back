ddc0e3aa66e11dbef5c866e0cd67c595e56bb5c3
RajBhut214 <rajbhut2832005@gmail.com>
2024-11-19 22:53:56 +0530

    added upstash

--- api/index.js
+++ api/index.js
@@ -2,25 +2,43 @@ import express from "express";
 import userrouter from "./routs/user.routes.js";
 import postrouter from "./routs/post.routes.js";
 import cors from "cors";
-import { rateLimit } from "express-rate-limit";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+const app = express();
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  limit: 100,
-  standardHeaders: "draft-7",
-  legacyHeaders: false,
-  trustProxy: true,
+const redis = new Redis({
+  url: process.env.KV_URL,
+  token: process.env.KV_REST_API_TOKEN,
+});
+const rateLimit = new Ratelimit({
+  redis,
+  limiter: Ratelimit.slidingWindow(20, "1m"), // 10 requests per minute
+  prefix: "my-rate-limit", // Prefix for the rate limit keys in Redis
 });
 
-const app = express();
-app.use(limiter);
+const rateLimiterMiddleware = async (req, res, next) => {
+  const identifier = req.ip;
+  const { success, remaining, reset } = await rateLimit.limit(identifier);
+
+  res.set("X-RateLimit-Limit", "10");
+  res.set("X-RateLimit-Remaining", remaining.toString());
+  res.set("X-RateLimit-Reset", reset.toString());
+
+  if (!success) {
+    res.status(429).send("Too many requests - try again later");
+    return;
+  }
+
+  next();
+};
+
 app.use(
   cors({
     origin: "https://paster.rajb.codes",
     credentials: true,
   })
 );
-
+app.use(rateLimiterMiddleware);
 app.use(express.json());
 app.use("/user", userrouter);
 app.use("/post", postrouter);
